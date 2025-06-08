fd8e94ce022ece81a5f3a3088e236d8f6af98594
nerdylua <nihaalsprakash.is23@rvce.edu.in>
2025-06-08 21:06:52 +0530

    Update readme

---
+++ getNewBento.ts
@@ -0,0 +1,21 @@
+const apiUrl = "https://opbento.edgexhq.tech/api/bento?n=Nihaal%20SP&g=nerdylua&x=nerdylua&l=nihaal-sp&i=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F150607136%3Fv%3D4&p=&z=bb61e";
+interface BentoResponse {
+  url: string;
+}
+
+const fetchBentoUrl = async (apiUrl: string): Promise<string> => {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data: BentoResponse = (await response.json()) as BentoResponse;
+    return data.url;
+  } catch (error) {
+    console.error("Error fetching Bento URL:", error);
+    throw error;
+  }
+};
+
+// @ts-ignore
+fetchBentoUrl(apiUrl);
