d655f6082989ecd9ce44fb56dc396036133ce040
nerdylua <nihaalsprakash.is23@rvce.edu.in>
2025-06-08 21:21:11 +0530

    Update readme

--- getNewBento.ts
+++ getNewBento.ts
@@ -1,4 +1,4 @@
-const apiUrl = "https://opbento.edgexhq.tech/api/bento?n=Nihaal%20SP&g=nerdylua&x=nerdylua&l=nihaal-sp&i=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F150607136%3Fv%3D4&p=&z=bb61e";
+const apiUrl = "https://opbento.edgexhq.tech/api/bento?n=Nihaal%20SP&g=nerdylua&x=nerdylua&l=nihaal-sp&i=https%3A%2F%2Favatars.githubusercontent.com%2Fu%2F150607136%3Fv%3D4&p=&z=d1529";
 interface BentoResponse {
   url: string;
 }
